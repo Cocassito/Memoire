0381be40620e8364a98bfc18f6c51b6be192628c
Alan.K <king.alan44@gmail.com>
2025-01-23 22:10:53 +0100

    ui

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['paths-js'], // Si une dépendance spécifique pose problème
+    esbuildOptions: {
+      // Cette option permet de gérer des dépendances qui ne respectent pas l'ESM
+      target: 'esnext',
+    },
+  },
+  build: {
+    target: 'esnext', // Assurez-vous que Vite cible ES Modules modernes
+
+    rollupOptions: {
+      output: {
+        format: 'esm', // Assurez-vous que la sortie est dans un format compatible
+      }
+    }
+  }
 })
